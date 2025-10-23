6a3f3ebce2473a77693702846a367ddb1b234c1e
KAVULURI ABHISHEK KUMAR REDDY <104247145+Abhishekkumarreddy485@users.noreply.github.com>
2025-10-23 12:16:29 +0530

    if its not pdf/image means we have to pass null

--- Backend/models/Quote.js
+++ Backend/models/Quote.js
@@ -4,10 +4,16 @@ const quoteSchema = new mongoose.Schema({
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
   text: { type: String },
-  imageUrl: { type: String },
-  imageId: { type: String }, // ✅ for Cloudinary public_id (needed for delete/update)
+  fileUrl: { type: String },
+  fileId: { type: String },
+  fileType: { 
+    type: String, 
+    enum: ['image', 'pdf', null],  // allow null
+    default: null                  // no fileType for text-only
+  },
   favorite: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Quote', quoteSchema);
+
